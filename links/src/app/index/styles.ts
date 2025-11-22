944701f7526d4d4056c8ca983a9a00192d06b44d
Layo Mendes <165064021+LayoMendes@users.noreply.github.com>
2025-11-22 22:27:28 +0000

    estilizacao

---
+++ links/src/app/index/styles.ts
@@ -0,0 +1,18 @@
+import {StyleSheet} from "react-native"
+import {colors} from "../../styles/colors"
+
+export const styles = StyleSheet.create({
+    container:{
+        flex: 1, 
+        backgroundColor: "black",
+         justifyContent: "center",
+          alignItems: "center",
+
+    },
+  
+    title: {
+        color: colors.green[300],
+        fontSize: 30,
+        
+    },
+})
